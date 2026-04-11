04b53d795ad57918157c907b33ba2ad2b91868f6
Francobarr10 <barrerafranco2005@gmail.com>
2026-04-11 16:22:19 -0300

    Agregar clases DNodo y ListaEnlazada en el paquete tdalista

---
+++ src/main/java/ar/edu/uns/cs/ed/tdas/tdalista/DNodo.java
@@ -0,0 +1,41 @@
+package ar.edu.uns.cs.ed.tdas.tdalista;
+
+import ar.edu.uns.cs.ed.tdas.Position;
+
+public class DNodo <E> implements Position<E> {
+    public DNodo<E> anterior;
+    public DNodo<E> siguiente;
+    public E elemento;
+
+    public DNodo(E e, DNodo<E> ant, DNodo<E> sig){
+        this.elemento=e;
+        this.anterior=ant;
+        this.siguiente=sig;
+    }   
+    public DNodo(E e){
+        this.elemento=e;
+        this.anterior=null;
+        this.siguiente=null;
+    }   
+    @Override
+    public E element() {
+        return this.elemento;
+    }
+    public void setElemento(E e){
+        this.elemento=e;
+    }
+    public DNodo<E> getAnterior() {
+        return anterior;
+    }
+    public void setAnterior(DNodo<E> anterior) {
+        this.anterior = anterior;
+    }
+    public DNodo<E> getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(DNodo<E> siguiente) {
+        this.siguiente = siguiente;
+    }
+
+
+}
